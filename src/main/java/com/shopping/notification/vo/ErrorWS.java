e4b4a0e3a1dbb1ebdd7679d08235bfd99ad22c89
peter86cu <39494828+peter86cu@users.noreply.github.com>
2023-12-14 00:31:41 -0300

    ok

---
+++ src/main/java/com/shopping/notification/vo/ErrorWS.java
@@ -0,0 +1,22 @@
+package com.shopping.notification.vo;
+
+public class ErrorWS {
+	
+	private String texto;
+	private ErrorState error;
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public ErrorState getError() {
+		return error;
+	}
+	public void setError(ErrorState error) {
+		this.error = error;
+	}
+	
+	
+
+}
